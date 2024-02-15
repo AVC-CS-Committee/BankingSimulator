66f16311cd0efe156ac26950c863150675b14b90
MBGooN <mbui2@students.avc.edu>
2024-02-14 19:42:09 -0800

    changed functionality of main menu
    added an area where the function of the banking app is able to go

--- Main.java
+++ Main.java
@@ -2,31 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char choice;
-
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n=============");
-            System.out.println("Menu:");
-            System.out.println("\n=============");
-            System.out.println("1. Enter the banking app.");
-            System.out.println("2. Exit. ");
-            System.out.println("Enter your choice (1 or 2): ");
-            choice = scanner.next().charAt(0);
+            displayMenu();
+            char choice = scanner.next().charAt(0);
 
             switch (choice) {
                 case '1':
-                    System.out.println("Entering the banking app.");
+                    enterBankApp();
                     break;
                 case '2':
                     System.out.println("Exiting the program.");
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Invalid choice. Please enter 1 or 2.");
-
+                    System.out.println("Invalid choice. Please enter 1 or 2.");         //Exception
             }
         }
-    } 
-}
+    }
+        
+    private static void displayMenu() {
+        System.out.println("\n================");
+        System.out.println("      Menu:");
+        System.out.println("================");
+        System.out.println("1. Enter the banking app.");
+        System.out.println("2. Exit. ");
+        System.out.println("Enter your choice (1 or 2): ");
+     }
+     private static void enterBankApp() {
+        System.out.println("Welcome to the Bank App!");
+        /*   FOR FUNCTIONALITY OF THE BANKING APP ADD HERE !!!!!!!!!!!!!!!!!!!!!!!!!! */ 
+        System.out.println("Thank you for using the app, Goodbye!");
+     }
+ }
